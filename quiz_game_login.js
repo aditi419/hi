a98e5aa5366be6648a833f5135e3bcac23ffa4d1
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-06-05 16:40:07 -0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,5 @@
+function addUser(){
+    player1 = document.getElementById("Player1_input");
+    player2 = document.getElementById("Player2_input");
+    window.location = "quiz_game_page.html";
+}
